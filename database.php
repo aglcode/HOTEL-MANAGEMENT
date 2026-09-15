2819fb9bd5fd684aca541828961f59917d5dadea
Angelo Almonte <160300504+aglcode@users.noreply.github.com>
2026-09-15 16:05:40 +0800

    Remove debug echo and die from database.php
    Removed debug output for database connection details.

--- database.php
+++ database.php
@@ -11,9 +11,6 @@ $db_name = getenv('MYSQLDATABASE');
 $username = getenv('MYSQLUSER');
 $password = getenv('MYSQLPASSWORD');
 
-echo "HOST: [$host] DB: [$db_name] USER: [$username] PASS_SET: " . (!empty($password) ? 'yes' : 'no');
-die();
-
 $conn = new mysqli($host, $username, $password, $db_name);
 
 // Check connection
